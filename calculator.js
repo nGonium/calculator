c6df9b0ce867f3c4c08b5bf8d145038b7e06235e
Didi nGon- <ngon-@hotmail.com>
2022-08-27 13:18:50 +0100

    Add event listeners

--- calculator.js
+++ calculator.js
@@ -1,8 +1,14 @@
+let value1;
+let operator;
+let value2;
+
+const isNumber = (num) => typeof num === 'number' && !Number.isNaN(num);
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-function operator(operator, a, b) {
+function operate(operator, a, b) {
+    console.log(operator, a, b);
     switch (operator) {
         case '+':
             return add(a, b)
@@ -21,7 +27,54 @@ function operator(operator, a, b) {
 
     }
 }
-// console.log(operator('+', 12, 12))
-// console.log(operator('-', 12, 12))
-// console.log(operator('*', 12, 12))
-// console.log(operator('/', 12, 12))
+
+function initEventListeners() {
+    const numbers = document.querySelectorAll('button[data-btn="number"]');
+    const operators = document.querySelectorAll('button[data-btn="operator"]');
+    const specials = {
+        equals: document.querySelector('button[data-btn="equals"]'),    
+        clear: document.querySelector('button[data-btn="clear"]'),    
+    };
+    const displayMain = document.querySelector('#display-main');
+    const displayPrev = document.querySelector('#display-prev');
+
+    
+    // Numbers
+    for(let num of numbers) {
+        num.addEventListener('click', e => {
+            // TODO: add protection against . and e
+            displayMain.textContent += num.value;
+        })
+    }
+
+    // Normal operators
+    for(let op of operators) {
+        op.addEventListener('click', e => {
+            if(!isNumber(value1)) {
+                value1 = Number(displayMain.textContent);
+                operator = op.value;
+                displayPrev.textContent = `${value1} ${operator}`;
+            } else {
+                value2 = Number(displayMain.textContent);
+                value1 = operate(operator, value1, value2);
+                operator = op.value;
+                displayPrev.textContent = `${value1} ${operator}`
+            }
+            displayMain.textContent = '';
+        })
+    }
+
+    // Special buttons
+    specials.equals.addEventListener('click', e => {
+        if(isNumber(value1) && operator) {
+            value2 = Number(displayMain.textContent);
+            displayPrev.textContent += ` ${value2} =`
+            displayMain.textContent = operate(operator, value1, value2);
+            value1 = undefined;
+        }
+    })
+} 
+
+addEventListener('DOMContentLoaded', (e) => {
+    initEventListeners();
+})
